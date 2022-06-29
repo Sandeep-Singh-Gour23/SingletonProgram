b8603034842dcac616b4ea72f3e29122e8834d65
Sandeep-Singh-Gour23 <sandeepsinghgour0@gmail.com>
2022-06-29 19:51:56 +0530

    Add project files.

---
+++ Counter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonProgram
+{
+    sealed class Counter
+    {
+        private static int test = 0;
+        static int count = 0;
+        private static Counter instance = null;
+        private Counter() {
+            test++;
+            Console.WriteLine("test value:" + test);    
+        }
+
+        public static Counter getInstance
+        {
+           get { 
+               if (instance == null)
+                    instance = new Counter();
+               return instance;
+            }
+        }
+
+        public void Increment()
+        {
+            count=count+1;
+        }
+        public void Decrement()
+        {
+            count=count-1;
+        }
+        public void CurrentCount()
+        {
+            Console.WriteLine("Current Count is" + count);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SingletonProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter obj = Counter.getInstance;
+            obj.Increment();
+            obj.Decrement();
+            obj.Increment();
+            obj.Increment();
+            obj.Increment();
+            obj.Increment();
+            obj.CurrentCount();
+
+
+
+        }
+    }
+}
